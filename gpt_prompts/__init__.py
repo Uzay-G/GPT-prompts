2b77ac9d55b090678f0b5fcda54987f50695231f
Uzay-G <uzgirit@gmail.com>
2023-03-06 18:11:12 -0500

    correct bugs, v0.1.3

--- gpt_prompts/__init__.py
+++ gpt_prompts/__init__.py
@@ -7,18 +7,9 @@ from pathlib import Path
 import yaml
 
 from langchain.chat_models import ChatOpenAI
-from langchain import PromptTemplate
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
+from langchain import PromptTemplate, LLMChain
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
 
 # init CLI
 cli = ArgumentParser(
@@ -28,11 +19,15 @@ cli = ArgumentParser(
 
 subparsers = cli.add_subparsers(dest="subcommand")
 
-sample_command = {"prompt": "This is a random prompt for an AI model. Here is my input: !argv1"}
+sample_command = {
+    "prompt": "This is a random prompt for an AI model. Here is my input: !argv1"
+}
+
 
 def file_replace(fmatch):
     with open(fmatch.group(1), "r") as f:
         return f.read()
+
 
 config_path = Path.home() / ".local/share/gpt_cli/config.yml"
 if not config_path.exists():
@@ -50,6 +45,20 @@ if not "key" in config or not config["key"]:
     sys.exit(1)
 
 chat = ChatOpenAI(temperature=0)
+OA_PROMPT = """Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand. Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics. Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+{history}
+Human: {prompt}
+"""
+
+TEMPLATE = PromptTemplate(input_variables=["history", "prompt"], template=OA_PROMPT)
+chain = LLMChain(
+    llm=chat,
+    prompt=TEMPLATE,
+    memory=ConversationalBufferWindowMemory(k=2),
+    verbose=True,
+)
+
 
 def enter_chat():
     while True:
@@ -72,8 +81,10 @@ for name, command in config["commands"].items():
         parser.add_argument(arg)
     parser.set_defaults(name=name)
 
+
 def prompt_chat(prompt):
-    return chat([HumanMessage(content=prompt)]).content
+    return chain.run(prompt=prompt)
+
 
 def main():
     args = cli.parse_args()
@@ -93,5 +104,6 @@ def main():
             prompt = prompt.replace(f"!{arg}", val)
         # match all file calls by searching for occurences of $filename
         prompt = re.sub(r"\$(\S+)", file_replace, prompt)
+        print(prompt)
         print(prompt_chat(prompt))
         enter_chat()
